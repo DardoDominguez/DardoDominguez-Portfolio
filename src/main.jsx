d08ae6ddd3b425d0af08365fe84fefd1fe934b0a
Dardo Dominguez <dardodominguez61@gmail.com>
2025-04-14 11:01:48 -0300

    actualización de la sección de habilidades

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App.jsx' // Adjust the path if App.jsx is in a subfolder like 'components'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
